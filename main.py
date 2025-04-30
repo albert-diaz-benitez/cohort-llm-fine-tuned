fefd652d1bde8d439c488159a2456e739d9580cc
Albert Diaz Benitez <albert.diaz@inari.io>
2025-04-30 16:51:03 +0200

    chore: working on fine tunning model

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import torch
+import torch.nn as nn
+import bitsandbytes as bnb
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, BitsAndBytesConfig
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.float16,
+)
+
+
 def main():
-    print("Hello from cohort-llm-fine-tuned!")
+    model_id = "NousResearch/Meta-Llama-3.1-8B-Instruct"
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        quantization_config=bnb_config,
+        device_map='auto',
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
+    print(model)
 
 
 if __name__ == "__main__":
